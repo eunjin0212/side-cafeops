5871da7c186e57e6b9979a6d204aa592da20bf4b
EunJin Kim <eunjin9639@gmail.com>
2026-08-14 23:40:37 -0400

    feat: reset profile selection and search input on successful score entry

--- src/app/(app)/scores/entry.tsx
+++ src/app/(app)/scores/entry.tsx
@@ -121,6 +121,8 @@ export default function ScoreEntryScreen() {
       },
       {
         onSuccess: () => {
+          setSelectedProfileIds([]);
+          setEmployeeSearch('');
           setSelectedCategoryIds([]);
           setNotes('');
           setImageUris([]);
